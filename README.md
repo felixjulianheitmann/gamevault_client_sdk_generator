58f1bbcc869f92ff9e615b08f6f0ad7dc16d327a
Felix Bruns <felix@bruns.hamburg>
2025-03-23 14:39:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gamevault_client_sdk
+Gamevault Client SDK for dart generated via OpenAPI Generator
---
+++ lib/api_gen.dart
@@ -0,0 +1,13 @@
+// Openapi Generator last run: : 2025-03-23T14:32:46.414299
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+  additionalProperties: AdditionalProperties(
+    sourceFolder: 'gamevault_api',
+  ),
+  inputSpec: InputSpec(path: 'gamevault_api.json'),
+  generatorName: Generator.dart,
+  outputDirectory: 'sdk',
+  skipSpecValidation: true,
+)
+class GvApi {}
