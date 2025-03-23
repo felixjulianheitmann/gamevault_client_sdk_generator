609f03eb34f92ea9d40cf76ca1842c7fa237588b
Felix Bruns <felix@bruns.hamburg>
2025-03-23 14:53:34 +0100

    changed metadata

--- lib/api.dart
+++ lib/api.dart
@@ -1,9 +1,15 @@
-// Openapi Generator last run: : 2025-03-23T14:45:52.851369
+// Openapi Generator last run: : 2025-03-23T14:52:17.496795
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
   additionalProperties: AdditionalProperties(
     sourceFolder: 'gamevault_api',
+      pubName: 'gamevault_client_sdk',
+      pubVersion: '0.0.1',
+      pubHomepage:
+          'https://github.com/felixjulianheitmann/gamevault_client_sdk.git',
+      pubDescription:
+          'Just the auto-generated client sdk for the gamevault API'
   ),
   inputSpec: InputSpec(path: 'gamevault_api.json'),
   generatorName: Generator.dart,
